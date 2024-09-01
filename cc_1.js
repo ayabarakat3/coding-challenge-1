a79b6d687981ac32c4269789fea169e351f27489
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-01 18:00:12 -0400

    task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -7,3 +7,15 @@ var isActive = true;
 console.log(employeeName, typeof(employeeName));
 console.log(employeeID, typeof(employeeID));
 console.log(isActive, typeof(isActive)); 
+
+
+
+// Task 2: Compound Data Types
+let products = ["Mascara", "Lipstick", "Blush"];
+const prouctDetails = {
+    price: 34.00,
+    name: products: 
+    inStock: 300,
+
+}
+console.log(products, productDetails);
