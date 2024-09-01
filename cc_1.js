b4f20afa5b773f75d834232a249052af0b3d88dc
ayabarakat3 <ayabarakat14@gmail.com>
2024-09-01 17:44:39 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables snd Data Types
+
+let employeeName = "Anthony Smith";
+const employeeID = 333;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive)); 
